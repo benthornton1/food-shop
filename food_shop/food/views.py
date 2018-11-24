262f9855d472cc0dcccfc4f5af3ab17f289eddce
Unknown <ben14367@hotmail.com>
2018-11-24 15:17:23 +0000

    Added more to food.

--- food_shop/food/views.py
+++ food_shop/food/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Food
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    food_list = Food.objects.order_by('-name')
+    template = 'food/index.html'
+    context = {
+        'food_list': food_list
+    }
+    return render(request, template, context)
+
+def detail(request, food_id):
+    food = get_object_or_404(Food, pk=food_id)
+    return render(request, 'food/detail.html', {'food': food})
