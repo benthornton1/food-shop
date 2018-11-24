057bb9ecd922b3a8bd88f91c676479a3f0f63eb0
David Sampson <dcsampsonlaptop@gmail.com>
2018-11-24 14:36:39 +0000

    Fixing urls, getting static files working

--- food_shop/cart/urls.py
+++ food_shop/cart/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
 from . import views
 
-urlpatters = [
-	url(r'^$', views.cart_detail, name='cart_detail'),
-    url(r'^add/(?P<book_id>\d+)/$', views.cart_add, name='cart_add'),
-    url(r'^remove/(?P<book_id>\d+)/$', views.cart_remove, name='cart_remove'),
-    url(r'^clear/$', views.cart_clear, name='cart_clear'),
+urlpatterns = [
+#	url(r'^$', views.cart_detail, name='cart_detail'),
+#    url(r'^add/(?P<book_id>\d+)/$', views.cart_add, name='cart_add'),
+#    url(r'^remove/(?P<book_id>\d+)/$', views.cart_remove, name='cart_remove'),
+#    url(r'^clear/$', views.cart_clear, name='cart_clear'),
 ]
