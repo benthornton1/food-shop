175d0694a760c1d1c10424674528df23245a3af5
Unknown <ben14367@hotmail.com>
2018-11-24 16:18:29 +0000

    Made images work on detail page

--- food_shop/food/models.py
+++ food_shop/food/models.py
@@ -8,7 +8,7 @@ class Food(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=10)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='food/food_img', blank=True)
+    image = models.ImageField(upload_to='img', blank=True)
 
     def __str__(self):
         return self.name
--- food_shop/food_shop/urls.py
+++ food_shop/food_shop/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
     path('food/', include('food.urls'), name = 'food'),
     path('',include('food.urls'),name ='main')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
