47fe9722f64852eb05e1344d9e537c56e4ff0770
David Sampson <dcsampsonlaptop@gmail.com>
2018-11-24 15:53:08 +0000

    Sorted out sign up issues with account_profile table, needs migration

--- food_shop/account/urls.py
+++ food_shop/account/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from . import views as core_views
 
-
+app_name = 'account'
 urlpatterns = [
     # specify URL patterns for login, logout and new user registration ('signup')
     url(r'^login/$', auth_views.LoginView.as_view(template_name="account/login.html"), name="login"),
